456b1ddb47e0941cf9f657508401fddd40b9b769
Aayush Shrestha <aayushstha20@gmail.com>
2025-08-06 14:07:49 +0545

    added user for log in feature

---
+++ src/main/java/net/engineeringdigest/journalApp/entity/User.java
@@ -0,0 +1,27 @@
+package net.engineeringdigest.journalApp.entity;
+
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.Setter;
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+import java.util.ArrayList;
+import java.util.List;
+
+@Document(collection = "users")
+@Getter
+@Setter
+public class User {
+    @Id
+    private ObjectId id;
+    @Indexed(unique = true)
+    @NonNull
+    private String userName;
+    @NonNull
+    private String password;
+    @DBRef
+    private List<JournalEntry> journalEntries = new ArrayList<>();
+}
